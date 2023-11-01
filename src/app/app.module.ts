2005cc371dbd3fbcbe95e2b2f6aa79081f4b8b23
bhuwan <bkspoudel@gmail.com>
2023-11-01 14:09:53 +0100

    create a new angular app and delete the default and create one component transaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule
